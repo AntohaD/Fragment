6a62fde58c16ada42f62fac7a4aed3c49c9910c9
Anton Dzherdzh <antondzherdzh@gmail.com>
2018-07-25 01:31:32 +0300

    Initial commit

---
+++ app/src/main/java/com/dzherdzh/anton/fragment/MainActivity.java
@@ -0,0 +1,33 @@
+package com.dzherdzh.anton.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.buttonStart);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonStart:
+                Intent intent = new Intent (this, ActivityForFragment.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+}
